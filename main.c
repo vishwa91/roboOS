6fe45385061d880e5a0bed4432b3b2d995333ff4
vishwanath <vishwa.hyd@gmail.com>
2012-05-24 19:19:59 +0530

    test.c renamed. No other change

---
+++ main.c
@@ -0,0 +1,19 @@
+// Author		vishwanath<vishwa.hyd@gmail.com>
+// Simple firmware to blink an LED with 1s clock and 50% duty cycle
+// This program will be used to help write the initial Makefile
+
+#include<avr/io.h>
+#define F_CPU 1000000U	// The compiler gives a warning if this is not defined
+#include<util/delay.h>
+
+int main(void)
+{
+	DDRB |= (1 << PB0 );	// make PB0 output. PORTA does not exist!
+	while(1)
+	{
+		PORTB |= (1 << PB0);
+		_delay_ms(500);
+		PORTB &= ~(1 << PB0);
+		_delay_ms(500);
+	}
+}
