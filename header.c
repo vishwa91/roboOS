5e23ee663dccac0fdfbdf77799bea337766d09c1
vishwanath <vishwa.hyd@gmail.com>
2012-07-06 23:23:16 +0530

    Top lines for every file

---
+++ header.c
@@ -0,0 +1,16 @@
+/************************************************
+Copyright(c) 2012 The roboOS open source project.
+
+This software is compatible with GPLv3. For 
+further support, visit:
+http://www.gnu.org/copyleft/gpl.html
+
+=================================================
+
+Module: <Module name here>
+Dependencies: <Dependent files here>
+
+Description: <What the file does>
+
+Note: <Notes if any>.
+************************************************/
