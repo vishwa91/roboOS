496b8fb365a77532cc6801029f29924677c1b94a
vishwanath <vishwa.hyd@gmail.com>
2012-07-07 00:32:19 +0530

    Added comment heading for functions

--- header.c
+++ header.c
@@ -1,16 +1,28 @@
-/************************************************
-Copyright(c) 2012 The roboOS open source project.
+/**************************************************************
+ Copyright(c) 2012 The roboOS open source project.
 
-This software is compatible with GPLv3. For 
-further support, visit:
-http://www.gnu.org/copyleft/gpl.html
+ This software is compatible with GPLv3. For further support,
+ visit:
+ http://www.gnu.org/copyleft/gpl.html
 
-=================================================
+===============================================================
 
-Module: <Module name here>
-Dependencies: <Dependent files here>
+ Module: <Module name here>
+ Dependencies: <Dependent files here>
 
-Description: <What the file does>
+ Description: <What the file does>
 
-Note: <Notes if any>.
-************************************************/
+ Note: <Notes if any>.
+**************************************************************/
+
+----------------------------------------------------------------
+----------------------------------------------------------------
+
+/************************************************************** 
+ Name : <Function name here>
+ Description: <A short intro>
+ Parameters: 
+ <What the function is expecting as input>
+ Return: <What the function returns>
+ Comments: <Self explanatory>
+ *************************************************************/
